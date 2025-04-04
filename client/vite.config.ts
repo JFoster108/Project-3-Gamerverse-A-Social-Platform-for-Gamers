2f95e8db41e8070a119da4f53f1214a8dc36659e
Justice Foster <omario2005x@gmail.com>
2025-04-03 20:21:14 -0400

    changed the index.html, and vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +9,10 @@ export default defineConfig({
         changeOrigin: true,
         secure: false
       }
+    },
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; font-src 'self' data: https:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'"
     }
   }
 })
